08b2d065194d3becde2d07deb97843520fd40dd5
timble <timblemichael20@gmail.com>
2022-09-14 09:36:16 +1000

    changing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'popes/index'
-      get 'popes/show'
+      resources :popes, only: [:index, :show, :create]
     end
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
-f 
